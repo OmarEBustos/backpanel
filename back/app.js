b323a718615878b6cb110074c7b1a1f68917c13a
OmarEBustos <omarbustos2013@gmail.com>
2024-02-06 17:51:41 -0300

    cabio

--- back/app.js
+++ back/app.js
@@ -1,22 +1,38 @@
 var express = require('express');
 var mongoose = require ("mongoose");
+var dotenv = require ('dotenv')
+
+
+
+
 var bodyparser = require ('body-parser');
 var port = process.env.port || 4201;
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
 var app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, { 
-    cors:{ origin:'https://paneladmin-sandy.vercel.app'}
- });
 
+dotenv.config()
+
+const httpServer = createServer(app);
+
+
+
+const io = new Server(httpServer, { 
+    cors:{origin: 'https://paneladmin-sandy.vercel.app'}
+   
+ });
+//origin:'https://paneladmin-sandy.vercel.app'
 io.on("connection", (socket) => {
   // ...
   socket.on('send_cart',function(data){
     io.emit('listen_cart',data);
   });
 });
+
+
+
+
 
 var cliente_router = require('./routes/cliente');
 var usuario_router = require('./routes/usuario');
@@ -29,12 +45,16 @@ app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyparser.json({limit: '50mb', extended: true}));
 
 mongoose.connect('mongodb+srv://Root:TiendaJD9!@cluster0.epkrue0.mongodb.net/?retryWrites=true&w=majority/tiendaprueba');
+
 httpServer.listen(port,function(){
     console.log('Servidor corriendo '+port);
 });
 
+// arrancar la app
+
+
 app.use((req,res,next) =>{
-    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Origin','https://paneladmin-sandy.vercel.app');
     res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE,OPTIONS');
     res.header('Allow','GET, PUT, POST, DELETE,OPTIONS');
